e2ac96aa776965a93d894f6f6ebc55928875048f
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-06-05 07:52:33 +0530

    Added biggest of two numbers.

--- src/main/java/com/careerit/jfs/cj/day1/BiggestOfTwoNumbers.java
+++ src/main/java/com/careerit/jfs/cj/day1/BiggestOfTwoNumbers.java
@@ -5,7 +5,7 @@ public class BiggestOfTwoNumbers {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-        int biggest = num1 > num2 ? num1 : num2;
+        int biggest = Math.max(num1, num2);
         System.out.println("Biggest of " + num1 + " and " + num2 + " is: " + biggest);
     }
 }
