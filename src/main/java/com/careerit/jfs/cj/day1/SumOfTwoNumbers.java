e6726f42529a15fe85929c8759017d3ef4e26578
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-06-05 08:13:41 +0530

    Added extra comment

--- src/main/java/com/careerit/jfs/cj/day1/SumOfTwoNumbers.java
+++ src/main/java/com/careerit/jfs/cj/day1/SumOfTwoNumbers.java
@@ -5,6 +5,7 @@ public class SumOfTwoNumbers {
         int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
+        // Print the result
         System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
