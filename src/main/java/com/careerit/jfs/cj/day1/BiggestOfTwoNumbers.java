49e89bc0e25928df6121e7d326bae3419922f7ee
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-06-05 07:50:15 +0530

    Update BiggestOfTwoNumbers.java

--- src/main/java/com/careerit/jfs/cj/day1/BiggestOfTwoNumbers.java
+++ src/main/java/com/careerit/jfs/cj/day1/BiggestOfTwoNumbers.java
@@ -5,14 +5,7 @@ public class BiggestOfTwoNumbers {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-        int biggest;
-
-        if (num1 > num2) {
-            biggest = num1;
-        } else {
-            biggest = num2;
-        }
-
+        int biggest = num1 > num2 ? num1 : num2;
         System.out.println("Biggest of " + num1 + " and " + num2 + " is: " + biggest);
     }
 }
