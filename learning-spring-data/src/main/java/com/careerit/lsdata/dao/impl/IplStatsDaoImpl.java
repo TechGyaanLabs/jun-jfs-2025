436b55acf344bcab6b503d84711286ad1fe4e4bc
avuthuamrutha28 <avuthuamrutha@gmail.com>
2025-09-08 10:17:22 +0530

    Removing jooq

--- learning-spring-data/src/main/java/com/careerit/lsdata/dao/impl/IplStatsDaoImpl.java
+++ learning-spring-data/src/main/java/com/careerit/lsdata/dao/impl/IplStatsDaoImpl.java
@@ -8,26 +8,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.careerit.lsdata.jooq.tables.TeamDetails.TEAM_DETAILS;
-import static com.careerit.lsdata.jooq.tables.PlayerDetails.PLAYER_DETAILS;
-import static org.jooq.impl.DSL.*;   // for count, sum, max, min, avg
-
 @Repository
 @RequiredArgsConstructor
+
 public class IplStatsDaoImpl implements IplStatsDao {
 
     private final DSLContext dsl;
-
     @Override
     public TeamStats getTeamStats(String teamLabel) {
         return dsl.select(
-                        TEAM_DETAILS.TEAM_LABEL,
-                        TEAM_DETAILS.TEAM_NAME,
-                        count(PLAYER_DETAILS.ID).as("totalPlayers"),
-                        sum(PLAYER_DETAILS.AMOUNT).as("totalAmount"),
-                        max(PLAYER_DETAILS.AMOUNT).as("maxAmount"),
-                        min(PLAYER_DETAILS.AMOUNT).as("minAmount"),
-                        avg(PLAYER_DETAILS.AMOUNT).as("averageAmount")
+                        team_details.team_label,
+                        team_details.team_name,
+                        count(player_details.ID).as("totalPlayers"),
+                        sum(player_details.AMOUNT).as("totalAmount"),
+                        max(player_details.AMOUNT).as("maxAmount"),
+                        min(player_details.AMOUNT).as("minAmount"),
+                        avg(player_details.AMOUNT).as("averageAmount")
                 )
                 .from(TEAM_DETAILS)
                 .leftJoin(PLAYER_DETAILS).on(TEAM_DETAILS.ID.eq(PLAYER_DETAILS.TEAM_ID))
